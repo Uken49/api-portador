5b499cf2930c30c17f46a4549e9d9e3bd397d0d0
uken49 <heldergavioes@hotmail.com>
2023-06-21 14:03:46 -0300

    feat: add mappers

---
+++ src/main/java/com/example/apiportador/infrastructure/mapper/BankAccountMapper.java
@@ -0,0 +1,13 @@
+package com.example.apiportador.infrastructure.mapper;
+
+import com.example.apiportador.applicationservice.domain.entity.BankAccount;
+import com.example.apiportador.presentation.request.CardHolderRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface BankAccountMapper {
+
+    BankAccount fromDomain(CardHolderRequest.BankAccount bankAccountRequest);
+
+}
