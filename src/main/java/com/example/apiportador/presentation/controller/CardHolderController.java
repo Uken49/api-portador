93d7f2d7010b230139f8dbffaa8acc6b8ffbcf58
uken49 <heldergavioes@hotmail.com>
2023-06-23 09:46:57 -0300

    refactor: add versão no request mapping

--- src/main/java/com/example/apiportador/presentation/controller/CardHolderController.java
+++ src/main/java/com/example/apiportador/presentation/controller/CardHolderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/card-holders")
+@RequestMapping("v1.0/card-holders")
 @RequiredArgsConstructor
 @Validated
 public class CardHolderController {
