14422e2473b6c902ed19fd5fd83d110adca8a8ec
uken49 <heldergavioes@hotmail.com>
2023-06-23 09:44:11 -0300

    refactor: add not null e unique nos atributos

--- data/data.sql
+++ data/data.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS CARD_HOLDER(
         id UUID UNIQUE,
-        client_id UUID UNIQUE,
-        status VARCHAR(8),
-        credit_limit DOUBLE PRECISION,
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
+        client_id UUID NOT NULL UNIQUE,
+        status VARCHAR(8) NOT NULL,
+        credit_limit DOUBLE PRECISION NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
         PRIMARY KEY (id)
 );
 
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS BANK_ACCOUNT(
         account CHAR(10),
         agency CHAR(4),
         bank_code CHAR(3),
-        card_holder_id UUID UNIQUE,
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
+        card_holder_id UUID NOT NULL UNIQUE,
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
         PRIMARY KEY (id),
         CONSTRAINT fk_card_holder_iid FOREIGN KEY (card_holder_id) REFERENCES CARD_HOLDER (id)
 );
@@ -23,12 +23,12 @@ CREATE TABLE IF NOT EXISTS BANK_ACCOUNT(
 CREATE TABLE IF NOT EXISTS CARD(
         id UUID UNIQUE,
         PRIMARY KEY (id),
-        credit_limit DOUBLE PRECISION,
-        card_number VARCHAR(19),
-        cvv INTEGER,
-        due_date DATE,
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-        card_holder_id UUID UNIQUE,
+        credit_limit DOUBLE PRECISION NOT NULL,
+        card_number VARCHAR(19) NOT NULL,
+        cvv INTEGER NOT NULL,
+        due_date DATE NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
+        card_holder_id UUID NOT NULL UNIQUE,
         CONSTRAINT fk_card_holder_id FOREIGN KEY (card_holder_id) REFERENCES CARD_HOLDER (id)
 );
