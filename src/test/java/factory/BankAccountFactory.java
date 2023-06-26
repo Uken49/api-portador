b6f07b23839e0b28450aba5224887ac33f7978c3
uken49 <heldergavioes@hotmail.com>
2023-06-26 12:13:27 -0300

    test: trocando instancias por toBuilder nas factorys

--- src/test/java/factory/BankAccountFactory.java
+++ src/test/java/factory/BankAccountFactory.java
@@ -1,18 +1,13 @@
 package factory;
 
 import com.example.apiportador.infrastructure.repository.entity.BankAccountEntity;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 public class BankAccountFactory {
     public static BankAccountEntity bankAccount() {
-        return new BankAccountEntity(
-                UUID.fromString("e4c2b603-d146-4759-bb49-8826b7d65749"),
-                "27184771-6",
-                "1121",
-                "302",
-                LocalDateTime.of(2023, 6, 22, 13, 55),
-                LocalDateTime.of(2023, 6, 22, 13, 55)
-        );
+        return BankAccountEntity.builder()
+                .account("27184771-6")
+                .agency("1121")
+                .bankCode("302")
+                .build();
     }
 }
