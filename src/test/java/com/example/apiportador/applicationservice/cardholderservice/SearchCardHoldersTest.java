8f4db72d6c4cabe01c733ca3bd90d29885dc84bd
uken49 <heldergavioes@hotmail.com>
2023-06-26 11:25:45 -0300

    test: criando testes da pesquisa

---
+++ src/test/java/com/example/apiportador/applicationservice/cardholderservice/SearchCardHoldersTest.java
@@ -0,0 +1,61 @@
+package com.example.apiportador.applicationservice.cardholderservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import com.example.apiportador.infrastructure.mapper.CardHolderMapper;
+import com.example.apiportador.infrastructure.mapper.CardHolderMapperImpl;
+import com.example.apiportador.infrastructure.repository.CardHolderRepository;
+import com.example.apiportador.presentation.handler.exception.InvalidStatusValueException;
+import com.example.apiportador.presentation.response.CardHolderResponse;
+import com.example.apiportador.util.enums.StatusEnum;
+import factory.CardHolderEntityFactory;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SearchCardHoldersTest {
+
+    @Mock
+    private CardHolderRepository cardHolderRepository;
+    @Spy
+    private CardHolderMapper cardHolderMapper = new CardHolderMapperImpl();
+    @InjectMocks
+    private SearchCardHolders searchCardHolders;
+
+    @Captor
+
+    private ArgumentCaptor<StatusEnum> statusArgumentCaptor;
+
+    @Test
+    void should_do_search_when_passing_status_like_active_and_inactive(){
+        when(cardHolderRepository.findByStatus(statusArgumentCaptor.capture())).thenReturn(List.of(CardHolderEntityFactory.cardHolderEntity()));
+        searchCardHolders.getAllCardHolders("active");
+        assertEquals("ACTIVE", statusArgumentCaptor.getValue().toString());
+
+        searchCardHolders.getAllCardHolders("inactive");
+        assertEquals("INACTIVE", statusArgumentCaptor.getValue().toString());
+
+    }
+
+    @Test
+    void should_map_as_CardHolderEntity_to_CardHolderResponse(){
+        when(cardHolderRepository.findAll()).thenReturn(List.of(CardHolderEntityFactory.cardHolderEntity()));
+        assertInstanceOf(CardHolderResponse.class, searchCardHolders.getAllCardHolders(null).get(0));
+    }
+
+    @Test
+    void should_throw_InvalidStatusValueException_when_status_is_invalid(){
+        assertThrows(InvalidStatusValueException.class,
+                () -> searchCardHolders.getAllCardHolders("plinio"), "Apenas os status 'active' e 'inactive' são aceitos");
+    }
+}
