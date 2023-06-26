7cf41153018933ce1b35685a36d8cc37fb6a7c4c
uken49 <heldergavioes@hotmail.com>
2023-06-26 11:38:42 -0300

    feat: add findByStatus na repository

--- src/main/java/com/example/apiportador/infrastructure/repository/CardHolderRepository.java
+++ src/main/java/com/example/apiportador/infrastructure/repository/CardHolderRepository.java
@@ -1,6 +1,8 @@
 package com.example.apiportador.infrastructure.repository;
 
 import com.example.apiportador.infrastructure.repository.entity.CardHolderEntity;
+import com.example.apiportador.util.enums.StatusEnum;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface CardHolderRepository extends JpaRepository<CardHolderEntity, UUID> {
 
     boolean existsByClientId(UUID clientId);
+
+    List<CardHolderEntity> findByStatus(StatusEnum status);
+
 }
