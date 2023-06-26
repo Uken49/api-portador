90cac5823ec6566e759f00933197619dcd370158
uken49 <heldergavioes@hotmail.com>
2023-06-26 14:53:48 -0300

    test: criando testes da consulta de portador por id

--- src/test/java/com/example/apiportador/applicationservice/cardholderservice/SearchCardHoldersTest.java
+++ src/test/java/com/example/apiportador/applicationservice/cardholderservice/SearchCardHoldersTest.java
@@ -8,11 +8,14 @@ import static org.mockito.Mockito.when;
 import com.example.apiportador.infrastructure.mapper.CardHolderMapper;
 import com.example.apiportador.infrastructure.mapper.CardHolderMapperImpl;
 import com.example.apiportador.infrastructure.repository.CardHolderRepository;
+import com.example.apiportador.presentation.handler.exception.CardHolderNotFoundException;
 import com.example.apiportador.presentation.handler.exception.InvalidStatusValueException;
 import com.example.apiportador.presentation.response.CardHolderResponse;
 import com.example.apiportador.util.enums.StatusEnum;
 import factory.CardHolderEntityFactory;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -48,7 +51,7 @@ class SearchCardHoldersTest {
     }
 
     @Test
-    void should_map_as_CardHolderEntity_to_CardHolderResponse(){
+    void should_map_as_CardHolderEntity_to_CardHolderResponse_getAllCardHolders(){
         when(cardHolderRepository.findAll()).thenReturn(List.of(CardHolderEntityFactory.cardHolderEntity()));
         assertInstanceOf(CardHolderResponse.class, searchCardHolders.getAllCardHolders(null).get(0));
     }
@@ -57,5 +60,21 @@ class SearchCardHoldersTest {
     void should_throw_InvalidStatusValueException_when_status_is_invalid(){
         assertThrows(InvalidStatusValueException.class,
                 () -> searchCardHolders.getAllCardHolders("plinio"), "Apenas os status 'active' e 'inactive' são aceitos");
+    }
+
+    @Test
+    void should_map_as_CardHolderEntity_to_CardHolderResponse_getCardHolderById(){
+        final UUID id = UUID.randomUUID();
+
+        when(cardHolderRepository.findById(id)).thenReturn(Optional.of(CardHolderEntityFactory.cardHolderEntity()));
+        assertInstanceOf(CardHolderResponse.class, searchCardHolders.getCardHolderById(id));
+    }
+
+    @Test
+    void should_throw_CardHolderNotFoundException_when_not_found_card_holder(){
+        final UUID id = UUID.randomUUID();
+
+        assertThrows(CardHolderNotFoundException.class,
+                () -> searchCardHolders.getCardHolderById(id), "Portador com ID: '%s' não foi encontrado".formatted(id));
     }
 }
