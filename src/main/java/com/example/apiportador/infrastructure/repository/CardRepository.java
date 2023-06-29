02f425ac5550621ecbe423d5b584bee7ad295371
uken49 <heldergavioes@hotmail.com>
2023-06-29 15:43:55 -0300

    feat: criando endpoint do get dos cartões de um portador

--- src/main/java/com/example/apiportador/infrastructure/repository/CardRepository.java
+++ src/main/java/com/example/apiportador/infrastructure/repository/CardRepository.java
@@ -1,6 +1,7 @@
 package com.example.apiportador.infrastructure.repository;
 
 import com.example.apiportador.infrastructure.repository.entity.CardEntity;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +13,12 @@ public interface CardRepository extends JpaRepository<CardEntity, UUID> {
 
     @Query(nativeQuery = true, value =
             "SELECT COALESCE(SUM(c.credit_limit), 0) AS sum, "
-            + "COALESCE(ch.credit_limit, 0) AS limit "
-            + "FROM card_holder ch "
-            + "LEFT JOIN card c ON ch.id = c.card_holder_id "
-            + "WHERE ch.id = ? "
-            + "GROUP BY ch.id")
+                    + "COALESCE(ch.credit_limit, 0) AS limit "
+                    + "FROM card_holder ch "
+                    + "LEFT JOIN card c ON ch.id = c.card_holder_id "
+                    + "WHERE ch.id = ? "
+                    + "GROUP BY ch.id")
     Optional<CardRepositoyResponse> findByCreditLimitAndCardHolderLimit(UUID cardHolderId);
+
+    List<CardEntity> findByCardHolderId(UUID cardHolderId);
 }
