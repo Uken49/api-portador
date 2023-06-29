94fe3f56d35180415b8d4ca66ea9ed99fb95e65d
uken49 <heldergavioes@hotmail.com>
2023-06-29 10:19:51 -0300

    feat: criando query

---
+++ src/main/java/com/example/apiportador/infrastructure/repository/CardRepositoyResponse.java
@@ -0,0 +1,9 @@
+package com.example.apiportador.infrastructure.repository;
+
+import java.math.BigDecimal;
+
+public interface CardRepositoyResponse {
+    BigDecimal getSum();
+
+    BigDecimal getLimit();
+}
