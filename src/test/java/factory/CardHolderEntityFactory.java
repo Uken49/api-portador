51354f6e8fecac9b972d1cfd4d4c449498804b63
uken49 <heldergavioes@hotmail.com>
2023-06-30 10:23:20 -0300

    test: add testes da consulta de por id do cartão

--- src/test/java/factory/CardHolderEntityFactory.java
+++ src/test/java/factory/CardHolderEntityFactory.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 public class CardHolderEntityFactory {
     public static CardHolderEntity cardHolderEntity() {
         return CardHolderEntity.builder()
+                .id(UUID.fromString("3309c846-6958-4f8b-b4a5-4b68ebb21327"))
                 .clientId(UUID.fromString("92ecd0a8-a3be-4f36-bfeb-dbe3aadde0b3"))
                 .creditAnalysisId(UUID.fromString("55ae2f2a-2480-4178-936d-686e8d7f731e"))
                 .status(StatusEnum.ACTIVE)
